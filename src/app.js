1872c9b1cb8cd97ab68e091644bfec9c319e91f9
Alexander Castro <alexander_3_4@msn.com>
2024-08-28 11:20:58 -0500

    corse

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors= require('cors')
 const app = express()
 
+
+app.use(cors());
 //importar rutas
 const productRoutes = require('./routes/product.routes');
 //conectarnos a mongoose
